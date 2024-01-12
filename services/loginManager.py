5cb159d8ded095075f4b192d1a86f40cc9ba4dab
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-01-12 19:11:56 +0100

    connection temporaire

--- services/loginManager.py
+++ services/loginManager.py
@@ -1,8 +1,8 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from services.jwtManager import JWTManager as jwtm
-from services.securityManager import SecurityManager as sm
+from services.jwtManager import JWTManager as JWTm
+from services.securityManager import SecurityManager as Sm
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -10,7 +10,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 class LoginManager:
     @staticmethod
     def getCurrentUser(token: str = Depends(oauth2_scheme)) -> str:
-        decoded_token = jwtm.decodeJWT(token)
+        decoded_token = JWTm.decodeJWT(token)
         if not decoded_token:
             raise HTTPException(status_code=401, detail="Invalid authentication credentials")
 
@@ -21,19 +21,28 @@ class LoginManager:
         return user
 
     @staticmethod
-    def login(form_data: OAuth2PasswordRequestForm = Depends()):
-
+    def login(form_data: OAuth2PasswordRequestForm):
+        print(form_data)
         form_mail = form_data.username
-        form_mail = sm.encrypt_data(form_mail)
+        form_mail = Sm.encrypt_data(form_mail)
 
         # Récupération de l'utilisateur
-        user = "db.query(User).filter(User.mail == form_mail).first()"
+        # user = "db.query(User).filter(User.mail == form_mail).first()"
+        user = {
+            "id": 1,
+            "pseudo": "pseudo",
+            "mail": "mail",
+            "password": "password"
+        }
         if not user:
             raise HTTPException(status_code=401, detail="incorrect username or password")
 
-        if not sm.verify_password(form_data.password, user.password):
+        # if not Sm.verify_password(form_data.password, user.password):
+        #     raise HTTPException(status_code=401, detail="Incorrect username or password")
+        if not form_data.password == user["password"]:
             raise HTTPException(status_code=401, detail="Incorrect username or password")
 
         # on crée le token
-        token = jwtm.encodeJWT(data={"sub": user.pseudo, "id": user.id, "mail": user.mail})
+        # token = JWTm.encodeJWT(data={"sub": user.pseudo, "id": user.id, "mail": user.mail})
+        token = JWTm.encodeJWT(data={"sub": user["pseudo"], "id": user["id"], "mail": user["mail"]})
         return {"access_token": token, "token_type": "bearer"}
