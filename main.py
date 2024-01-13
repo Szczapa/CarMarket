5556535453c19be1a373157830b97f85e9b02af3
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-01-13 15:38:51 +0100

    routeur user
    ajout des routeur de catégorie user

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import login_router, product_router
+from routers import login_router, product_router, user_router
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
@@ -12,4 +12,4 @@ async def root():
 
 app.include_router(login_router.router, tags=["login"])
 app.include_router(product_router.router, tags=["product"])
-app.include_router(product_router.router, tags=["product"])
+app.include_router(user_router.router, tags=["user"])
