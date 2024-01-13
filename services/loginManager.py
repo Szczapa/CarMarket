bb048edfa99ecb7d9026d668191dee2554e5fd66
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-01-13 15:41:36 +0100

    finalisation du système de connection de base

--- services/loginManager.py
+++ services/loginManager.py
@@ -1,6 +1,9 @@
 from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
+from models.user import User
+from services.databaseManager import get_db
 from services.jwtManager import JWTManager as JWTm
 from services.securityManager import SecurityManager as Sm
 
@@ -9,40 +12,19 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 class LoginManager:
     @staticmethod
-    def getCurrentUser(token: str = Depends(oauth2_scheme)) -> str:
-        decoded_token = JWTm.decodeJWT(token)
-        if not decoded_token:
-            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
-
-        # Récupération de l'utilisateur
-        user = "db.query(User).filter(User.id == decoded_token.get('id')).first()"
+    def login(form_data: OAuth2PasswordRequestForm, db: Session):
+        # {
+        #     "pseudo": "Eduardo",
+        #     "mail": "Eduardo@gmail.com",
+        #     "password": "Eduardo1234",
+        #     "first_name": "Eduardo",
+        #     "last_name": "Miguelito"
+        # }
+        form_mail = Sm.hash_mail(form_data.username)
+        user = db.query(User).filter(User.hash_mail == form_mail).first()
         if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        return user
-
-    @staticmethod
-    def login(form_data: OAuth2PasswordRequestForm):
-        print(form_data)
-        form_mail = form_data.username
-        form_mail = Sm.encrypt_data(form_mail)
-
-        # Récupération de l'utilisateur
-        # user = "db.query(User).filter(User.mail == form_mail).first()"
-        user = {
-            "id": 1,
-            "pseudo": "pseudo",
-            "mail": "mail",
-            "password": "password"
-        }
-        if not user:
-            raise HTTPException(status_code=401, detail="incorrect username or password")
-
-        # if not Sm.verify_password(form_data.password, user.password):
-        #     raise HTTPException(status_code=401, detail="Incorrect username or password")
-        if not form_data.password == user["password"]:
+            raise HTTPException(status_code=401, detail="No user found")
+        if not Sm.verify_password(form_data.password, user.password):
             raise HTTPException(status_code=401, detail="Incorrect username or password")
-
-        # on crée le token
-        # token = JWTm.encodeJWT(data={"sub": user.pseudo, "id": user.id, "mail": user.mail})
-        token = JWTm.encodeJWT(data={"sub": user["pseudo"], "id": user["id"], "mail": user["mail"]})
+        token = JWTm.encodeJWT(data={"sub": user.pseudo, "id": user.id, "mail": user.mail})
         return {"access_token": token, "token_type": "bearer"}
