8843776cacb87fe36976004e525e5f0fd39a89b2
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-01-23 11:49:37 +0100

    ajout router

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
-from routers import login_router, product_router, user_router
+from routers import login_router, product_router, user_router, category_router
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# origins = [
+#     "http://localhost:3000",
+# ]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get("/")
@@ -13,3 +25,4 @@ async def root():
 app.include_router(login_router.router, tags=["login"])
 app.include_router(product_router.router, tags=["product"])
 app.include_router(user_router.router, tags=["user"])
+app.include_router(category_router.router, tags=["category"])
