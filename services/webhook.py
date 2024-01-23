76685131a3020e3b3c9cc441cd9cd0da13e85e8c
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-01-23 11:50:20 +0100

    système de webhook

---
+++ services/webhook.py
@@ -0,0 +1,47 @@
+import os
+import requests
+import json
+
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+webhook_url = os.getenv("WEBHOOK_URL")
+if webhook_url is None:
+    raise ValueError("L'url du webhook est introuvable")
+
+
+class WebhookManager:
+    @staticmethod
+    def productWebhook(data):
+        information = [
+            {
+                "color": "11206535",
+                "title": "Demande de Requête produit",
+                "fields": [
+                    {"name": "Nom du produit :", "value": "value", "inline": False},
+                    {"name": "Client :", "value": "value", "inline": False},
+                    {"name": "Prix :", "value": "value", "inline": False},
+                ],
+                "footer": {
+                    "text": datetime.now().strftime("%d/%m/%Y [%X]")
+                }
+            }
+        ]
+
+        data = {
+            "embeds": information
+        }
+
+        # Headers
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # Performing the POST request
+        response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
+        if response.status_code != 204:
+            raise ValueError(
+                f"Request to Discord returned an error {response.status_code}, the response is:\n{response.text}")
+        return response.status_code
