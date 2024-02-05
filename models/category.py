ebc209b8debfddc176d5b37f4ab2b5ccb76e9c8f
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-02-06 00:56:03 +0100

    modification des model de produit et category

--- models/category.py
+++ models/category.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from services.databaseManager import Base
 
@@ -8,6 +9,7 @@ class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
     name = Column(String(255), unique=True)
+    products = relationship("Product", back_populates="category_rel")
 
     def __repr__(self):
         return '<Category %r>' % (self.name)
--- models/product.py
+++ models/product.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
 
-from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String, Boolean, Numeric
+from pydantic import BaseModel, Field
+from sqlalchemy import Column, Integer, String, Boolean, Numeric, ForeignKey
+from sqlalchemy.orm import relationship
 
 from services.databaseManager import Base
 
@@ -9,14 +10,17 @@ from services.databaseManager import Base
 class Product(Base):
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), unique=False)
-    description = Column(String(255), unique=False)
-    price = Column(Numeric, unique=False)
-    category = Column(Integer, unique=False)
-    note = Column(Integer, unique=False, default=0)
-    discount_amount = Column(Integer, unique=False, default=0)
-    active_discount_price = Column(Boolean, unique=False, default=False)
-    active_product = Column(Boolean, unique=False, default=True)
+    name = Column(String(255))
+    description = Column(String(255))
+    price = Column(Numeric)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    note = Column(Integer, default=0)
+    discount_amount = Column(Integer, default=0)
+    active_discount_price = Column(Boolean, default=False)
+    active_product = Column(Boolean, default=True)
+
+    # Ajoutez une relation ici
+    category_rel = relationship("Category", back_populates="products")
 
     def __repr__(self):
         return '<Product %r>' % (self.name)
@@ -27,3 +31,11 @@ class ProductCreate(BaseModel):
     description: str
     price: Decimal
     category: int
+
+
+class ProductUpdate(BaseModel):
+    discount_amount: float = None
+    active_discount_price: bool = None
+    active_product: bool = None
+    note: int = None
+
